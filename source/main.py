1b782a8beb1b71d10477da29599e3984ac0e5be4
gwon9906 <gwon99065@naver.com>
2025-04-27 19:59:33 +0900

    기본 송신 및 수신 모드 함수 제거, 메뉴 선택 기능 단순화

--- source/main.py
+++ source/main.py
@@ -1,30 +1,3 @@
-#main .py
-
-
-
-def run_transmitter():
-    pass
-
-
-def run_receiver():
-    pass
-
-def choose_mode():
-    print("LoRa SYSTEM")
-    print("1. Transmitter")
-    print("2. Receiver")
-    choice = input("Choose mode (1 or 2): ")
-    return choice
-
-if __name__ == "__main__":
-    mode = choose_mode()
-    if mode == '1':
-        run_transmitter()
-    elif mode == '2':
-        run_receiver()
-    else:
-        print("Invalid choice. Please run the program again.")
-
 """
 Entry‑point for the ChirpChirp LoRa data system.
 
